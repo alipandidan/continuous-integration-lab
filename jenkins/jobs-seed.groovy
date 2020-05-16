b7e58df7734840bf5f949712d3948add92745528
Ali Pandidan <ali.pandidan@tobii.com>
2020-05-16 18:57:53 +0200

    Create first job DSL seed

---
+++ jenkins/jobs-seed.groovy
@@ -0,0 +1,25 @@
+multibranchPipelineJob('TestJob') {
+branchSources {
+    github {
+        // The id option in the Git and GitHub branch source contexts is now mandatory (JENKINS-43693).
+        id('12312313') // IMPORTANT: use a constant and unique identifier
+        scanCredentialsId('github-credentials')
+        repoOwner('pandidan')
+        repository('swedbank-exercise')
+
+        traits {
+            BranchDiscoveryTrait {
+                strategyId(1)
+            }
+        }
+    }
+}
+orphanedItemStrategy {
+    discardOldItems {
+        numToKeep(10)
+    }
+}
+// triggers {
+//     periodic(5)
+// }
+}
