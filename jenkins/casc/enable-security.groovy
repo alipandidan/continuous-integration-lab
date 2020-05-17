ca971aae19787e2c649d27558bae59fe3d049145
Ali Pandidan <ali.pandidan@tobii.com>
2020-05-17 18:17:33 +0200

    Utilize JCasC groovy extention to enable a security realm

---
+++ jenkins/casc/enable-security.groovy
@@ -0,0 +1,16 @@
+import jenkins.model.*
+import hudson.security.*
+
+def adminUsername = System.getenv("JENKINS_ADMIN_USER")
+def adminPassword = System.getenv("JENKINS_ADMIN_PASS")
+assert adminPassword != null : "No JENKINS_ADMIN_USER env var provided, but required"
+assert adminPassword != null : "No JENKINS_ADMIN_PASS env var provided, but required"
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount(adminUsername, adminPassword)
+Jenkins.instance.setSecurityRealm(hudsonRealm)
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+strategy.setAllowAnonymousRead(false)
+Jenkins.instance.setAuthorizationStrategy(strategy)
+
+Jenkins.instance.save()
